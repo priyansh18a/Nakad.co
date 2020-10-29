e185122a33d3f69f7f1463f42c7af1ccb89ee50e
Harshil Lodhi <lodhi.harshil@gmail.com>
2020-10-29 16:27:56 +0000

    removed complex logic

--- src/routes/listTier2InvoicesForApproval.ts
+++ src/routes/listTier2InvoicesForApproval.ts
@@ -9,37 +9,12 @@ export async function listTier2InvoicesForApproval(req, res) {
 }
 
 async function listTier2InvoicesForApprovalInternal(tier1Id: Number): Promise<Tier2Invoice[]> {
-    const anchorInvoices = await getConnection().createQueryBuilder()
-        .select('AI')
-        .from(AnchorInvoice, 'AI')
-        .leftJoin(AnchorTier2InvoiceMapping, 'ATM', '"AI"."InvoiceId" = "ATM"."AnchorInvoiceId"')
-        .where('"ATM"."AnchorInvoiceId" IS NULL').andWhere('"AI"."Tier1Id" = :id', { id: tier1Id })
-        .orderBy('"AI"."DueDate"', 'ASC')
-        .getMany();
-    console.log("anchorInvoices: " + JSON.stringify(anchorInvoices));
-
     const tier2Invoices = await getConnection().createQueryBuilder()
         .select('t')
         .from(Tier2Invoice, 't')
-        .where('"t"."ApprovalStatus" = \'Pending\'')
+        .where('"t"."ApprovalStatus" = \'Pending\'').andWhere('"t"."Tier1Id" = :id', {id: tier1Id})
         .orderBy('"t"."DueDate"', 'ASC').getMany();
 
     console.log("tier2Invoices: " + JSON.stringify(tier2Invoices));
-
-    let j = 0;
-    let invoiceToReturn = [];
-    // TODO(harshil) This logic will need change.
-    for (let i = 0; i < tier2Invoices.length; i++) {
-        const inv: Tier2Invoice = tier2Invoices[i];
-
-        if (j >= anchorInvoices.length)
-            break;
-        const anchorInvoice = anchorInvoices[j];
-        if (inv.dueDate < anchorInvoice.dueDate && inv.invoiceAmount.lessThanOrEqual(anchorInvoice.invoiceAmount)) {
-            invoiceToReturn.push(inv);
-            j++;
-        }
-    }
-
-    return invoiceToReturn;
+    return tier2Invoices;
 }
