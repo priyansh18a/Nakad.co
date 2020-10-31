fdfe94a22a12a71d7921fd18824fae97b9018cb7
Harshil Lodhi <lodhi.harshil@gmail.com>
2020-10-31 13:02:03 +0000

    tslint fixes

--- src/routes/DiscountedTier2Invoice.ts
+++ src/routes/DiscountedTier2Invoice.ts
@@ -2,14 +2,14 @@ import { AnchorInvoice } from "../database/entity/AnchorInvoice";
 import { Tier2Invoice } from "../database/entity/Tier2Invoice";
 
 export interface PartAnchorInvoice {
-    anchorInvoice: AnchorInvoice;
-    partialAmount: Dinero.Dinero;
+  anchorInvoice: AnchorInvoice;
+  partialAmount: Dinero.Dinero;
 }
 
 export interface DiscountedTier2Invoice {
-    tier2Invoice: Tier2Invoice;
-    partAnchorInvoices: PartAnchorInvoice[];
-    discountedAmount: Dinero.Dinero;
-    discountedAnnualRatePercentage: number;
-    status: "Pending" | "Discounted";
+  tier2Invoice: Tier2Invoice;
+  partAnchorInvoices: PartAnchorInvoice[];
+  discountedAmount: Dinero.Dinero;
+  discountedAnnualRatePercentage: number;
+  status: "Pending" | "Discounted";
 }
--- src/routes/listTier2InvoicesForApproval.ts
+++ src/routes/listTier2InvoicesForApproval.ts
@@ -4,17 +4,20 @@ import { Tier2Invoice } from "../database/entity/Tier2Invoice";
 
 // Request has query param named tier1Id
 export async function listTier2InvoicesForApproval(req: Request, res: Response) {
-    const tier1Id = parseInt(req.query.tier1Id[0], 10);
-    return res.json(await listTier2InvoicesForApprovalInternal(tier1Id));
+  const tier1Id = parseInt(req.query.tier1Id[0], 10);
+  return res.json(await listTier2InvoicesForApprovalInternal(tier1Id));
 }
 
 async function listTier2InvoicesForApprovalInternal(tier1Id: number): Promise<Tier2Invoice[]> {
-    const tier2Invoices = await getConnection().createQueryBuilder()
-        .select('t')
-        .from(Tier2Invoice, 't')
-        .where('"t"."ApprovalStatus" = \'Pending\'').andWhere('"t"."Tier1Id" = :id', {id: tier1Id})
-        .orderBy('"t"."DueDate"', 'ASC').getMany();
+  const tier2Invoices = await getConnection()
+    .createQueryBuilder()
+    .select("t")
+    .from(Tier2Invoice, "t")
+    .where('"t"."ApprovalStatus" = \'Pending\'')
+    .andWhere('"t"."Tier1Id" = :id', { id: tier1Id })
+    .orderBy('"t"."DueDate"', "ASC")
+    .getMany();
 
-    console.log("tier2Invoices: " + JSON.stringify(tier2Invoices));
-    return tier2Invoices;
+  console.log("tier2Invoices: " + JSON.stringify(tier2Invoices));
+  return tier2Invoices;
 }
